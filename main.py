50e81ce341ee17aa13b98fd10c112402ee364ca1
Liguo Ma <maliguo@outlook.com>
2025-11-26 22:54:33 +0800

    feat:first commit after github repo init

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-from typing import Union
+# Pydantic 示例
+
+from enum import Enum
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"hello": "World"}
+@app.get("/user/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/user/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+# Enum 示例
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the image"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
